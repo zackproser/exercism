aee6ee75dc306b237fbf048d6080c1159868159f
Zachary Proser <zackproser@gmail.com>
2022-06-11 13:04:41 -0400

    Check in solution to Parallel Letter Frequency

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -16,5 +16,23 @@ func Frequency(s string) FreqMap {
 // ConcurrentFrequency counts the frequency of each rune in the given strings,
 // by making use of concurrency.
 func ConcurrentFrequency(l []string) FreqMap {
-	panic("Implement the ConcurrentFrequency function")
+
+	// Create a channel to receive FreqMap results
+	res := make(chan FreqMap)
+
+	// Range over input strings, feeding each into the Frequency function, whose
+	// results are then sent into the results channel
+	for _, s := range l {
+		go func(s string) { res <- Frequency(s) }(s)
+	}
+
+	finalCounts := FreqMap{}
+
+	// Iterate over all results, incrementing counts in the final FreqMap
+	for range l {
+		for c, count := range <-res {
+			finalCounts[c] += count
+		}
+	}
+	return finalCounts
 }
